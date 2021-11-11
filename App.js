7c26f5cfb87b351a6aec0db56fb50dd890db9a49
Mateus Ragazzi <mateusragazzi.b@gmail.com>
2021-11-10 21:49:43 -0400

    Ajustando arquitetura do projeto

--- App.js
+++ App.js
@@ -2,22 +2,21 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Home from './screens/Home/Index';
-import Login from './screens/Login/Index';
-import Register from './screens/Register/Index';
+import Home from './src/screens/Home/Index';
+import Login from './src/screens/Login/Index';
+import Register from './src/screens/Register/Index';
 
 const Stack = createNativeStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+export default class App extends React.Component {
+  render () {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Register" component={Register} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
-
-export default App;
