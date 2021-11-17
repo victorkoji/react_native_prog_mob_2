0e6bb4ca0115e2fa81a77712de7fbf8cf9abc44e
Mateus Ragazzi <mateusragazzi.b@gmail.com>
2021-11-16 21:09:44 -0400

    Atualizando objeto questionário

--- src/database/models/Questionnaires.ts
+++ src/database/models/Questionnaires.ts
@@ -1,50 +1,51 @@
 export class Questionnaires {
-  constructor(id?: number) {
-    this.id = id;
+  constructor() {
+    this.id = 1;
+    this.question = {
+      question_1: {
+        title: "Teste 1",
+        type: "text",
+        alternatives: []
+      },
+      question_2: {
+        title: "Teste 2",
+        type: "choice",
+        alternatives: [
+          {
+            title: "Teste 1"
+          },
+          {
+            title: "Teste 2"
+          },
+          {
+            title: "Teste 3"
+          },
+          {
+            title: "Teste 4"
+          }
+        ]
+      },
+      question_3: {
+        title: "Teste 3",
+        type: "choice",
+        alternatives: [
+          {
+            title: "Teste 1"
+          },
+          {
+            title: "Teste 2"
+          },
+          {
+            title: "Teste 3"
+          },
+          {
+            title: "Teste 4"
+          }
+        ]
+      }
+    }
   }
 
   public id: number;
-  public question = {
-    question_1: {
-      title: "Teste 1",
-      type: "text",
-      alternatives: []
-    },
-    question_2: {
-      title: "Teste 2",
-      type: "choice",
-      alternatives: [
-        {
-          title: "Teste 1"
-        },
-        {
-          title: "Teste 2"
-        },
-        {
-          title: "Teste 3"
-        },
-        {
-          title: "Teste 4"
-        }
-      ]
-    },
-    question_3: {
-      title: "Teste 3",
-      type: "choice",
-      alternatives: [
-        {
-          title: "Teste 1"
-        },
-        {
-          title: "Teste 2"
-        },
-        {
-          title: "Teste 3"
-        },
-        {
-          title: "Teste 4"
-        }
-      ]
-    }
-  }
+  public question: Object;
 }
