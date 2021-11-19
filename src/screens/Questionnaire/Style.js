e2ae563ab24406fa17ad9e95d081030ed31b9c81
Fernando Kendy <yahirokendy@gmail.com>
2021-11-18 22:58:07 -0400

    Criado o Template inicial do Questionário

--- src/screens/Questionnaire/Style.js
+++ src/screens/Questionnaire/Style.js
@@ -1,10 +1,48 @@
 import { StyleSheet } from 'react-native'
 
 export const Style = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+    questionnaire:{
+        // backgroundColor: '#F7F7F0',
+        // opacity: 37,
     },
+    text_question:{
+        paddingTop: 5,
+        fontSize: 15,
+        paddingBottom: 10,
+        paddingLeft: 10,
+    },
+    radionButtonSize:{
+        fontSize: 2,
+        marginBottom: 10,
+        marginLeft: 10
+    },
+    commentQuestions:{
+        textAlign: 'justify',
+        paddingLeft: 30,
+        paddingRight: 30,
+        paddingTop: 20,
+        paddingBottom: 20,
+        fontWeight: 'bold'
+    },
+    btnConfirmQuestionnaire:{
+        marginRight: 10
+    },
+    btnActionQuestionnaire: {
+        display: 'flex',
+        flexDirection: 'row-reverse',
+        marginLeft: 10,
+        marginRight: 10,
+        
+    },
+    segmentQuestion: {
+        backgroundColor: 'white',
+        marginLeft: 20,
+        marginRight: 20,
+        borderRadius: 15,
+        marginBottom: 20,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 5,
+        paddingRight: 5,
+    }
 });
--- src/screens/Questionnaire/index.js
+++ src/screens/Questionnaire/index.js
@@ -1,18 +1,105 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
-import { CheckBox } from 'react-native-ui-lib';
+import {Text, CheckBox, RadioButton, Colors, Button, RadioGroup} from 'react-native-ui-lib';
 import { Style } from './Style';
 
-export default function Questionnaire({navigation}) {
-    return (
-        <View style={Style.container}>
-            <CheckBox
-                center
-                title='Click Here'
-                checkedIcon='dot-circle-o'
-                uncheckedIcon='circle-o'
-                checked={1}
-            />
-        </View>
-    );
+const COLORS = {
+    DEFAULT: {name: 'Black', color: Colors.black},
+};
+
+export default class Questionnaire extends Component {
+    static colors = COLORS;
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: undefined,
+            messageType: undefined,
+            disabledSelectedValue: true
+        };
+
+        this.state2 = {
+            color: undefined,
+            messageType: undefined,
+            disabledSelectedValue: true
+        };
+
+        this.state3 = {
+            color: undefined,
+            messageType: undefined,
+            disabledSelectedValue: true
+        };
+    }
+
+    renderRadioButtonForColorEnum(color) {
+        return (
+            
+            <View style={Style.questionnaire} row centerV marginB-5>
+                <Text style={Style.commentQuestions}>
+                Certifique-se de responder adequadamente as perguntas a seguir
+                </Text>
+                <View style={Style.segmentQuestion}>
+                    <Text style={Style.text_question}>
+                    Exemplo Teste 1?
+                    </Text>
+                    <RadioGroup initialValue={this.state.color || null} onValueChange={value => this.setState({color: value})}>
+                        <RadioButton style={Style.radionButtonSize} size={20} value="1" label="Não"/>
+                        <RadioButton style={Style.radionButtonSize} size={20} value="2" label="Não 2"/>
+                        <RadioButton style={Style.radionButtonSize} size={20} value="3" label="Sim"/>
+                        <RadioButton style={Style.radionButtonSize} size={20} value="4" label="Sim 2"/>
+                    </RadioGroup>
+                </View>
+
+                <View style={Style.segmentQuestion}>
+                    <Text style={Style.text_question}>
+                    Exemplo Teste 2?
+                    </Text>
+                    <RadioGroup initialValue={this.state2.color || null} onValueChange={value => this.setState({color: value})}>
+                        <RadioButton style={Style.radionButtonSize} size={20} value="5" label="Não"/>
+                        <RadioButton style={Style.radionButtonSize} size={20} value="6" label="Não 2"/>
+                        <RadioButton style={Style.radionButtonSize} size={20} value="7" label="Sim"/>
+                        <RadioButton style={Style.radionButtonSize} size={20} value="8" label="Sim 2"/>
+                    </RadioGroup>
+                </View>
+                <View style={Style.segmentQuestion}>
+                    <Text style={Style.text_question}>
+                    Exemplo Teste 3?
+                    </Text>
+                    <RadioGroup initialValue={this.state3.color || null} onValueChange={value => this.setState({color: value})}>
+                        <RadioButton style={Style.radionButtonSize} size={20} value="9" label="Não"/>
+                        <RadioButton style={Style.radionButtonSize} size={20} value="10" label="Não 2"/>
+                        <RadioButton style={Style.radionButtonSize} size={20} value="11" label="Sim"/>
+                        <RadioButton style={Style.radionButtonSize} size={20} value="12" label="Sim 2"/>
+                    </RadioGroup>
+                </View>
+
+                <View style={Style.btnActionQuestionnaire}>
+                    {/* <Button
+                        label={'Cancelar'}
+                        enableShadow
+                        style={Style.btnConfirmQuestionnaire}
+                    /> */}
+                    <Button
+                        label="Cancelar"
+                        size={Button.sizes.medium}
+                        outline
+                        style={Style.btnConfirmQuestionnaire}
+                    />
+                    <Button
+                        label={'Confirmar'}
+                        enableShadow
+                        style={Style.btnConfirmQuestionnaire}
+                    />
+                </View>
+
+            </View>
+        );
+    }
+    
+    render(){
+        return (
+            <View >
+                {this.renderRadioButtonForColorEnum(Questionnaire.colors.DEFAULT)}
+            </View>
+        );
+    };
 }
